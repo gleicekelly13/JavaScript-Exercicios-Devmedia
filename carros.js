27065af09e0e5581968115b32be452d19b726d30
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-18 13:01:26 -0300

    Create carros.js

---
+++ carros.js
@@ -0,0 +1,16 @@
+let carros = [
+    { nome:"Corolla",   marca: "Toyota",        ano: 2018,  cor: "preto",       preco: 90000},
+    { nome:"Argo",      marca: "Fiat",          ano: 2020,  cor: "vermelho",    preco: 55000},
+    { nome:"Cívic",     marca: "Honda",         ano: 2015,  cor: "prata",       preco: 70000},
+    { nome:"Gol",       marca: "Volkswagen",    ano: 2019,  cor: "branco",      preco: 45000},
+    { nome:"Onix",      marca: "Chevrolet",     ano: 2020,  cor: "azul",        preco: 60000},
+];
+
+console.log(carros[2].nome);
+
+console.log(carros[1].marca);
+
+console.log(carros[3].preco);
+
+console.log(carros[4].cor);
+
