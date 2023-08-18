5743789dd0c73b5c35f647f7b86ad551f0c112d5
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-18 12:51:41 -0300

    Create agenda.js

---
+++ agenda.js
@@ -0,0 +1,14 @@
+const agenda = 1;
+let msg = "";
+
+if(agenda == 1)
+{
+    msg = "Hoje vou jogar vídeo game";
+}else if(agenda == 2)
+{
+    msg = "Hoje vou assistir um fime";
+}else 
+{
+    msg = "Hoje vou dormir";
+}
+console.log(msg);
