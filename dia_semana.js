eba862e527a9921eecd8ee8d0aa0f6e2a827ae70
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-19 16:46:31 -0300

    Create dia_semana.js

---
+++ dia_semana.js
@@ -0,0 +1,26 @@
+let dia_semana = 1;
+
+switch(dia_semana)
+{
+    case 1:
+        console.log("Domingo");
+    break;
+    case 2:
+        console.log("Segunda-feira");
+    break;
+    case 3:
+        console.log("Terça-feira");
+    break;
+    case 4:
+        console.log("Quarta-feira");
+    break;
+    case 5:
+        console.log("Quinta-feira");
+    break;
+    case 6:
+        console.log("Sexta-feira");
+    break;
+    case 7:
+        console.log("Sábado");
+    break;
+}
