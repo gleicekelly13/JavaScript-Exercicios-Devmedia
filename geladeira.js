a97881906d7857026ca61016c4b8b12f5aa9732b
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-20 12:59:34 -0300

    Create geladeira.js

---
+++ geladeira.js
@@ -0,0 +1,22 @@
+/*let produto = "liquidificador";
+let retorno = "";
+
+if(produto == "Geladeira") {
+    retorno = "desconto";
+}
+else
+{
+    retorno = "normal";
+}
+
+console.log(retorno); */
+
+//Transformando em if ternário
+
+/*let produto = "Geladeira";
+let retorno = (produto == "Geladeira") ? "desconto" : "normal";
+console.log(retorno); */
+
+let resposta = true;
+let resultado = (resposta) ? "Correta" : "Errada";
+console.log(resultado);
