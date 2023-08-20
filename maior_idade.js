a073e47d3bcfa1c13d2d8eee59860f191ed80142
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-20 12:58:53 -0300

    Create maior_idade.js

---
+++ maior_idade.js
@@ -0,0 +1,14 @@
+let idade = 20;
+/*let resultado = "";
+
+if(idade >= 18) {
+    resultado = "Maior de idade";
+}
+else
+{
+    resultado = "Menor de idade";
+}
+console.log(resultado); */
+
+let resultado = (idade >=18) ? "Maior de idade" : "Menor de idade";
+console.log(resultado);
