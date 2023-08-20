a0163dd1597c1fe08463c42c2c373a6200793e0d
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-20 12:57:23 -0300

    Create doramas.js

---
+++ doramas.js
@@ -0,0 +1,15 @@
+let colecao_doramas = [
+    { id: 1,  titulo: "Apostando Alto",     episodios: 16 },
+    { id: 2,  titulo: "Agency",          episodios: 12 },
+    { id: 3,  titulo: "No clima do amor",   episodios: 12 },
+];
+
+let primeiro_dorama = colecao_doramas[0];
+
+let id_dorama = primeiro_dorama.id;
+let titulo_dorama = primeiro_dorama.titulo;
+let episodios_dorama = primeiro_dorama.episodios;
+
+console.log("Id do dorama: " + id_dorama);
+console.log("Título do  dorama: " + titulo_dorama);
+console.log("Total de episódios do dorama: " + episodios_dorama + "\n");
