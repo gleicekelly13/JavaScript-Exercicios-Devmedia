156e9ea0ef7670e145a61a80899765dbfe1a70b8
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-21 13:48:38 -0300

    Create produto_cliente.js

---
+++ produto_cliente.js
@@ -0,0 +1,21 @@
+var produto = {
+    id: 9,
+    nome: "Cafeteira Elétrica",
+    valor: 99.00
+};
+
+console.log(produto.id);
+console.log(produto.nome);
+console.log(produto.valor);
+
+var cliente = {
+    id: 32,
+    nome: "Gleice kelly",
+    tel: "(99) 9999-9999"
+};
+
+console.log(cliente.id);
+console.log(cliente.nome);
+
+
+
