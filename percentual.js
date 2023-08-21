1eb014261ea40902769332e1c1c670ebecd7ee9b
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-21 13:47:51 -0300

    Create percentual.js

---
+++ percentual.js
@@ -0,0 +1,6 @@
+let valor = 20;
+let percentual = 10;
+
+let resultado = (valor * percentual) / 100;
+
+console.log("\nResultado: " + percentual + "% de " + valor + " é igual a: " + resultado + "\n");
