eaa034ec687149fc2bde3a4a2ae752ee4e36b718
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-22 14:23:44 -0300

    Create semaforo.js

---
+++ semaforo.js
@@ -0,0 +1,10 @@
+let semaforo = "verde";
+if(semaforo == "verde") {
+    console.log("Siga");
+}else if (semaforo == "amarelo") {
+    console.log("Atenção");
+}else if (semaforo == "vermelho") {
+    console.log("Pare");
+}else {
+    console.log("Comando inexistente");
+}
