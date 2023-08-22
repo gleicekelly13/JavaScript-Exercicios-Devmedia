f48701c2763509b725df3436fbf3ff6735b1cb34
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-21 22:17:20 -0300

    Create resposta_usuario.js

---
+++ resposta_usuario.js
@@ -0,0 +1,12 @@
+let resposta_usuario = " ";
+
+if(resposta_usuario == 0)
+{
+    console.log("resposta correta");
+}
+else
+{
+    console.log("resposta incorreta");
+}
+
+//Exemplo prático sobre uso de igualdade estrita
