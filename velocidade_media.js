8e4ccbd92b3fe3f0636a6ea1bbbf659d827f66f6
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-23 08:50:32 -0300

    Create velocidade_media.js

---
+++ velocidade_media.js
@@ -0,0 +1,10 @@
+let posicao_inicial = 5;
+let posicao_final = 23;
+
+let tempo = 6;
+
+let deslocamento = posicao_final - posicao_inicial;
+
+let velocidade_media = deslocamento / tempo;
+
+console.log("\nA  média foi de " + velocidade_media + "m/s\n");
