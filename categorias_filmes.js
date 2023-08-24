2c7cbf7221bad6ae14d48ef69937d7f08b5b37ce
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-24 19:56:35 -0300

    Create categorias_filmes.js

---
+++ categorias_filmes.js
@@ -0,0 +1,17 @@
+let categorias = [
+    {id: 1, nome: "Ficção científica"},
+    {id: 2, nome: "Aventura"},
+    {id:3, nome: "Comédia"},
+    {id:4, nome: "Terror"},
+    {id:5, nome: "Drama"}
+];
+
+for(let categoria of categorias) {
+
+    console.log(categoria.id);
+    console.log(categoria.nome);
+
+    console.log("\n");
+}
+
+//Imprime os dados das categorias.
