94b53cef7a718ad8584aa67ae721ce76fac77999
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-24 21:00:10 -0300

    Create contatos.js

---
+++ contatos.js
@@ -0,0 +1,14 @@
+let contatos = [
+    {id: 1, nome: "Daiana"},
+    {id: 2, nome: "Raquel"},
+    {id: 3, nome: "Diogo"},
+    {id: 4, nome: "Sandro"},
+    {id: 5, nome: "Carolina"}
+];
+
+for(let contato of contatos) {
+
+    console.log(contato.id);
+    console.log(contato.nome);
+
+}
