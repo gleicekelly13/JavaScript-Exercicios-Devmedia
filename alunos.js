36d766d3cadb08f1514e93616ab31c72a082facc
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-25 21:43:20 -0300

    Create alunos.js

---
+++ alunos.js
@@ -0,0 +1,8 @@
+let alunos = ["Carlos", "Pablo", "Juliana", "Ingrid"];
+
+let total_alunos = alunos.length;
+
+for(let contador = 0; contador < total_alunos; contador++) {
+
+    console.log(alunos[contador]);
+}
