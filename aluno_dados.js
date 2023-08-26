5f0f5b9e12133f58b4a1e794801138dab6d5fd65
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-25 21:46:37 -0300

    Update aluno_dados.js

--- aluno_dados.js
+++ aluno_dados.js
@@ -3,3 +3,6 @@ let aluno = { nome: "Marcos", email: "marcos@email.com", idade: 25};
 for(let dados in aluno) {
     console.log(aluno[dados]);
 }
+
+/*A estrutura for...in percorre os atributos do abjeto aluno.
+Todos os atributos são impressos.*/
