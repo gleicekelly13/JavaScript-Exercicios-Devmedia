a4459cf2653348ce3b13de629ec902db7d6fed45
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-08-25 21:42:50 -0300

    Create livros.js

---
+++ livros.js
@@ -0,0 +1,8 @@
+let livros = ["A única coisa", "O poder do hábito", "Geração de valor", "Hábitos atômicos"];
+
+let contador = 0;
+
+while(contador < livros.length){
+    console.log(livros[contador]);
+    contador++;
+}
