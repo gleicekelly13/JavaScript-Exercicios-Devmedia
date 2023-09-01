4864cb50519b5d6099537187354b8d36f34d3dbe
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-01 18:07:19 -0300

    Create noticiass.js

---
+++ noticiass.js
@@ -0,0 +1,10 @@
+let noticias = [
+    "Nubank levanta US$ 400 milhões em nova rodada de investimento",
+    "Procon-SP notifica Serasa pedindo explicações sobre vazamento de dados",
+    "Login no Whatsapp Web terá autenticação por biometria",
+    "Lucro da Microsoft cresce 32% no 4º trimestre de 2020"
+];
+
+console.log(noticias[0]); //Imprimindo a notícia sobre o nubank
+
+console.log(noticias.length); //Imprimindo o total de notícias do array
