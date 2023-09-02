7122efde3b22ffce99fb36db46768830e3e26289
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-02 13:22:09 -0300

    Create tabuada_de_8.js

---
+++ tabuada_de_8.js
@@ -0,0 +1,8 @@
+let multiplicador = 8;
+
+for(let contador = 0; contador <= 10; contador++) {
+
+    let resultado = multiplicador * contador;
+
+    console.log(multiplicador + " x " + contador + " = " + resultado);
+}
