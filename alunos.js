244768936baa959834fb0b1076107e1c6e9ae3cc
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-02 12:11:08 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -1,8 +1,8 @@
 let alunos = ["Carlos", "Pablo", "Juliana", "Ingrid"];
 
-let total_alunos = alunos.length;
+let total_alunos = alunos.length; //Total de alunos que estão no array.
 
 for(let contador = 0; contador < total_alunos; contador++) {
 
-    console.log(alunos[contador]);
+    console.log(alunos[contador]); //Imprime o nome do aluno a cada iteração
 }
