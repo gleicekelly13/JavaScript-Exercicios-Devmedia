374429e85aa90148c6c04b15a05bd11569d13c41
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-03 12:45:52 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -6,3 +6,15 @@ for(let contador = 0; contador < total_alunos; contador++) {
 
     console.log(alunos[contador]); //Imprime o nome do aluno a cada iteração
 }
+
+//-----------------------------------------------
+//WHILE
+/* = 0;
+
+while(contador < alunos.length) { //Acada execução do bloco será impresso o nome do aluno que está na posição contador no array alunos.
+
+    let aluno_nome = alunos[contador];
+
+    console.log("Aluno: " + aluno_nome);
+    contador++;
+} */
