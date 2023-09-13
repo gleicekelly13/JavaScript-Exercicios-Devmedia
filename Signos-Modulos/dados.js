c0021f42e6f42ba19d34df4f9a9ff3bf9238f20f
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-13 16:12:23 -0300

    Create dados.js

---
+++ Signos-Modulos/dados.js
@@ -0,0 +1,16 @@
+let signos = [ //Coleção de dados para os signos
+    {"Nome": "Aquário", "DataInicio": "01-21","DataFim":"02-19"}, //O formato das datas dos signos tem o mês como 1º número e o dia como 2º dia.
+    {"Nome": "Peixes", "DataInicio": "02-20","DataFim":"03-20"},
+    {"Nome": "Áries", "DataInicio": "03-21","DataFim":"04-20"},
+    {"Nome": "Touro", "DataInicio": "04-21","DataFim":"05-20"},
+    {"Nome": "Gêmeos", "DataInicio": "05-21","DataFim":"06-21"},
+    {"Nome": "Câncer", "DataInicio": "06-22","DataFim":"07-23"},
+    {"Nome": "Leão", "DataInicio": "07-24","DataFim":"08-23"},
+    {"Nome": "Virgem", "DataInicio": "08-24","DataFim":"09-23"},
+    {"Nome": "Libra", "DataInicio": "09-24","DataFim":"10-23"},
+    {"Nome": "Escorpião", "DataInicio": "10-24","DataFim":"11-22"},
+    {"Nome": "Sagitário", "DataInicio": "11-23","DataFim":"12-21"},
+    {"Nome": "Capricórnio", "DataInicio": "12-22","DataFim":"01-20"}
+   ];
+
+   export default signos; //exportando os dados do array
