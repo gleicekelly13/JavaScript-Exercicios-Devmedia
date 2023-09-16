61190be22f41379617aa44d779b34c1d7caf2cbe
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-16 09:54:07 -0300

    Create conversor.js

---
+++ conversor_celsius/conversor.js
@@ -0,0 +1,8 @@
+function converteParaCelsius(fahrenheit) {
+    let celsius = (fahrenheit - 32) / 1.8;
+    return celsius;
+}
+
+export default converteParaCelsius;
+
+//Camada que armazena a lógica de conversão de temperatura da aplicação
