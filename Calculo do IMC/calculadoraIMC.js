831c4dd1df8579da20e97d6fa5e27dde23403e4f
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-17 14:12:30 -0300

    Create calculadoraIMC.js

---
+++ Calculo do IMC/calculadoraIMC.js
@@ -0,0 +1,27 @@
+import formataIMC from "./formatacaoDados.js";
+
+function calculaIMC(peso, altura) {
+
+    let imc = peso / (altura * altura);
+    let imcFormatado = formataIMC(imc);
+
+    return imcFormatado;
+}
+
+function retornaStatusIMC(imc) { //Retorna um status do usuário com base no cáculo do IMC
+    let status;
+
+    if(imc <= 18.5) {
+        status = 'abaixo do peso';
+    } else if (imc > 18.5 && imc < 24.9) {
+        status = 'peso normal';
+    } else if (imc > 24.9 && imc < 30) {
+        status = 'acima do peso';
+    } else {
+        status = 'obeso';
+    }
+
+    return status;
+}
+
+export {calculaIMC, retornaStatusIMC};
