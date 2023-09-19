394feac2b647bdf3887520a16715de77cb727bc4
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-19 20:04:07 -0300

    Delete Signos-Modulos directory

--- Signos-Modulos/main.js
+++
@@ -1,9 +0,0 @@
-import signos from "./dados/dados.js";
-import retorna_signo from "./funcoes/funcoes.js";
-
-
-let data_app = new Date(); //Data atual do sistema
-   
-const nome_signo = retorna_signo(signos, data_app);
-   
-console.log("O signo de hoje é: " + nome_signo);
