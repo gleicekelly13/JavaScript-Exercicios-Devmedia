b5126a2fef0cb1154a6305cbb63d09fe9d0d38bf
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-19 13:31:11 -0300

    Create retornoSerie.js

---
+++ Série do dia/retornoSerie.js
@@ -0,0 +1,15 @@
+let serie = ['Smallville', 'Wandinha', 'Loki', 'Gothan', 'Arrow', 'Flash', 'DC Legends'];
+
+function retornaSerieDia(indexSerie) {  //Retorna o nome da série que corresponde ao índice passado no parâmetro
+    
+    return serie[indexSerie]; //Retorna uma série de acordo com o dia da semana
+}
+
+export default retornaSerieDia;
+
+
+
+
+
+
+//Objetivo: criar uma aplicação que sugerisse uma série de uma lista com base no dia da semana.
