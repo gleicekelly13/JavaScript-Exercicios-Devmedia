9702f4f77b6fbb473bd0836a64da67351d369f5d
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-20 14:49:12 -0300

    Create index.js

---
+++ serie_do_dia_refatorada/index.js
@@ -0,0 +1,7 @@
+import retornaSerieDia from "./retornoSerie.js";
+
+let serieDoDia = retornaSerieDia();
+
+console.log("Série do dia: " + serieDoDia);
+
+//A camada de exibição não precisa saber como o dado foi obtido, sua única preocupação é exibí-lo
