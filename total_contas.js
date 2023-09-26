71fb65e3ea1171b848f24813f98cdc54ae630c98
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2023-09-26 19:21:56 -0300

    Create total_contas.js

---
+++ total_contas.js
@@ -0,0 +1,19 @@
+const contas = [
+    {nome: 'Luz', valor: 250, vencimento: 1605279748399},
+    {nome: 'Celular', valor: 100, vencimento: 1603119748399},
+    {nome: 'Fatura Cartão', valor: 7600, vencimento: 1608390148399},
+];
+
+let totalAPagar = 0;
+const hoje = new Date();
+const timeHoje = hoje.getTime() / 1000;
+
+for(let i = 0; i < contas.length; i++){
+    const conta = contas[i];
+
+    if(conta.vencimento >= timeHoje) {
+        totalAPagar += conta.valor
+    }
+}
+
+console.log(totalAPagar);
