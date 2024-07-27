9ca3f8fb7326e650d1b1818986a5e30f870cb320
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-27 19:58:29 -0300

    Create email.js

---
+++ email.js
@@ -0,0 +1,10 @@
+let emails_alunos = [
+    "danilo@gmail.com",
+    "gleice@gmail.com",
+    "kelly@gmail.com",
+    "gustavo@gmail.com"
+];
+
+for(let email of emails_alunos) { //Estrutura de repetção que percorre o array
+    console.log(email);
+}
