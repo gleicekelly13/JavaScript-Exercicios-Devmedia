21b6bb19dfa443ada6f6b1c3ca69d05bfb3b0381
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-11-16 15:53:22 -0300

    Create substring.js

---
+++ substring.js
@@ -0,0 +1,9 @@
+let frase = "Eu sou programadora JavaScript";
+
+let resultado = frase.substring(7, 19);
+
+console.log(resultado);
+
+/*
+O método substring extrai a palavra programadora que está no conteúdo da variável frase.
+*/
