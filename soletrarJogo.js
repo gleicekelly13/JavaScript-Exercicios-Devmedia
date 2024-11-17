df84d03e8a26365a11f6fceaa004cf073cc79d3d
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-11-17 10:56:15 -0300

    Create soletrarJogo.js

---
+++ soletrarJogo.js
@@ -0,0 +1,7 @@
+const jogoDoAno = "Stronghold Crusader";
+let index = 0;
+
+do {
+    console.log(jogoDoAno[index]); //Soletra a variável jogoDoAno.
+    index++;
+} while(index < jogoDoAno.length); 
