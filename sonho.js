5034c012707179e548b552b8a8fd8772d6bb7360
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-11-18 16:21:43 -0300

    Create sonho.js

---
+++ sonho.js
@@ -0,0 +1,9 @@
+let frase = "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado."
+
+let posicao = frase.indexOf("sonho");
+
+console.log(posicao);
+
+/*O método indexOf retorna a posição da substring dentro da string.
+No caso acima, sonho está na posição 14 da variável frase.
+*/
