6a082e3b4441eba92f77a5d26cd61d17e4de296c
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-11-21 15:16:22 -0300

    Create nome.js

---
+++ nome.js
@@ -0,0 +1,10 @@
+let nome_composto = "Gleice Kelly";
+
+let array = nome_composto.split(" ");
+
+let nome = array[0];
+
+console.log("Olá " + nome + ", seja bem vinda");
+
+/* Obtemos o primeiro nome da string nome_composto.Com o método split() separamos a string em diversas substring e depois acessamos o primeiro índice do array, que contém o nome que desejamos.
+*/
