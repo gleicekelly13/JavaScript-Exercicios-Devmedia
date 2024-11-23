4f2d3646925b98450fd02de59bda662130f71b45
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-11-23 14:40:34 -0300

    Create meses_testes.js

---
+++ meses_testes.js
@@ -0,0 +1,14 @@
+//contador = 0;
+
+let meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+/*do {
+    console.log(meses[contador]); //Acessando valor do array pelo índice
+    contador++;
+}while(contador < meses.length); */
+
+//--------------------------------------------------
+//FOR
+for(contador = 0; contador < meses.length; contador++) {
+    console.log(meses[contador]);
+}
