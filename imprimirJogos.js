e0e18f631f62cd873482302401b71a2d1f66086e
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-11-27 20:58:41 -0300

    Create imprimirJogos.js

---
+++ imprimirJogos.js
@@ -0,0 +1,14 @@
+const jogos = ["Stronghold", "Stronghold Crusader", "Stronghold II"];
+
+let i = 0;
+
+while(i < jogos.length) {
+    console.log(jogos[i]);
+    i++;
+}
+
+
+/*
+Loop para imprimir os valores do array 'jogos 
+enquanto i for menor que a quantidade de elementos do array jogos.
+*/
