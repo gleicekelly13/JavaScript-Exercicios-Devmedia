f7a5a87ee346e25d5b10288a7e2fa70f381f2624
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-04 08:48:57 -0300

    Create function_imparPar.js

---
+++ function_imparPar.js
@@ -0,0 +1,21 @@
+const lista_numeros = [1, 2, 3, 7, 9, 12, 15, 18];
+
+const total_impares_pares = (numeros) => {
+
+    let impares = 0;
+    let pares = 0;
+
+    for(let numero of numeros) {
+
+        if(numero % 2 == 0) {
+            pares++;
+        } else {
+            impares++;
+        }
+    }
+
+    console.log("O total de números ímpares é: " + impares);
+    console.log("O total de números pares é: " + pares);
+}
+
+total_impares_pares(lista_numeros);
