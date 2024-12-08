749fe0fa28600c2a89f814daa12b37c43d1a8e02
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-08 09:51:44 -0300

    Create decremento_dowhile.js

---
+++ decremento_dowhile.js
@@ -0,0 +1,8 @@
+let contador = 10; //Variável contador inicializada com o valor 10.
+
+do {
+
+    console.log(contador); //Antes de tudo, o valor é impresso 
+    contador--; //decremento do contador
+
+}while(contador > 0); //condição, enquanto o contador for maior que 0, a estrutura deve continuar iterando.
