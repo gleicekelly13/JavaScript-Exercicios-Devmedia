1c19ed705658021390d73b7f189342023af3d0f3
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-19 12:48:48 -0300

    Create function_mensalidade.js

---
+++ function_mensalidade.js
@@ -0,0 +1,8 @@
+function retorna_mensalidade() {
+    return 50;
+}
+
+console.log("O valor da mensalidade é: " + retorna_mensalidade().toFixed(2));
+
+/*let valor_mensalidade = retorna_mensalidade();
+console.log("O valor da mensalidade é: " + valor_mensalidade.toFixed(2)); */
