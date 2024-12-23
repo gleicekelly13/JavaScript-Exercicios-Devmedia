22d36097a3c48555a11e9b7de1f7d0d2e522f2fc
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-23 09:48:37 -0300

    Create series.js

---
+++ series.js
@@ -0,0 +1,10 @@
+const imprime_itens = (series) => {
+    
+    for (let serie of series) {
+        console.log(serie);
+    }
+}
+
+let series = ['Greys anatomy', 'Station 19', 'Suits', 'Apostando Alto',];
+
+imprime_itens(series);
