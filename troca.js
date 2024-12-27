e609034a11bcba1580e4f335cd9a8c4753769021
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-12-27 11:31:27 -0300

    Create troca.js

---
+++ troca.js
@@ -0,0 +1,7 @@
+let texto = "João Pessoa é uma das cidades mais arborizadas do mundo!";
+
+let resultado = texto.replace("João Pessoa", "JP");
+
+console.log(resultado);
+
+//A substring `João Pessoa` será trocada pela substring `JP`.
